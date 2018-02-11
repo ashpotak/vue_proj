016581f77b91f71354ce0dec5e89a46627869d13
ashpotak <alexandr.shpotak@gmail.com>
2018-02-12 00:37:19 +0200

    First commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Rating from './components/Rating'
+
+new Vue({
+  el: '#app',
+  template: '<Rating :grade="0" :maxStars="5" :hasCounter="true"/>',
+  components: {
+    Rating
+  }
+})
+
+
